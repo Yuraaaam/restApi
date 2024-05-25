fb2c53c43c0b3e595e6c38ebafd7a75a6531bc51
YuriyMikheev <112696898+Yuraaaam@users.noreply.github.com>
2024-05-25 12:07:30 +0300

    Add files via upload

---
+++ restApiProject/internal/http-server/handlers/url/save/save.go
@@ -0,0 +1,51 @@
+package save
+
+import (
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"golang.org/x/exp/slog"
+	"net/http"
+	resp "restApiProject/internal/lib/api/response"
+	"restApiProject/internal/lib/logger/sl"
+)
+
+type Request struct {
+	URL   string `json:"url" validate:"required,url"`
+	Alias string `json:"alias,omitempty"`
+}
+
+type Response struct {
+	resp.Response
+	Alias string `json:"alias"`
+}
+
+type URLSaver interface {
+	SaveURL(urlToSave string, alias string) (int64, error)
+}
+
+func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.url.save.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to decode request"))
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("request", req))
+
+		//
+
+	}
+}
